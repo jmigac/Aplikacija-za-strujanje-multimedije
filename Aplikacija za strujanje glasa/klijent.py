6fb2732abb8083d73f6dd50fc02b2ea530c4d7c4
Jurica Migač <jurica1.migac@gmail.com>
2019-12-01 17:13:43 +0100

    02 Implementirana funkcionalnost klijenta za slanje glasa ka serveru. #2
    U potpunosti implementiran program klijentskog dijela za slanje glasa preko mikrofona ka serveru.

--- Aplikacija za strujanje glasa/klijent.py
+++ Aplikacija za strujanje glasa/klijent.py
@@ -14,3 +14,21 @@ FORMAT = pyaudio.paInt16
 HOST = "127.0.0.1"
 PORT = 953
 SIZE = 1024
+
+if __name__ == '__main__':
+    p = pyaudio.PyAudio()
+    strujanje = p.open(
+        format = FORMAT,
+        channels = KANALI,
+        rate = RATE,
+        input = INPUT,
+        frames_per_buffer = CHUNK)
+    veza = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while 1:
+        podaci = strujanje.read(CHUNK)
+        veza.sendto(podaci,(HOST,PORT))
+    
+    veza.close()
+    strujanje.stop_stream()
+    strujanje.close()
+    p.terminate()
