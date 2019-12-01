c5ad6c8631f45645bacbfe613561ffdb9ccbb708
Jurica Migač <jurica1.migac@gmail.com>
2019-12-01 17:21:21 +0100

    03 Implementirana funkcionalnost servera za primanje glasa. #3
    U potpunosti implementiran program serverskog dijela za primanje glasa mikrofona od klijenta.

--- Aplikacija za strujanje glasa/server.py
+++ Aplikacija za strujanje glasa/server.py
@@ -14,3 +14,22 @@ HOST = "127.0.0.1"
 PORT = 953
 BACKLOG = 1
 SIZE = 4096
+
+if __name__ == '__main__':
+    p = pyaudio.PyAudio()
+    strujanje = p.open(
+        format=FORMAT,
+        channels = KANALI,
+        rate = RATE,
+        output = OUTPUT)
+    veza = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    veza.bind((HOST,PORT))
+    while True:
+        podaci,adresaKlijenta = veza.recvfrom(SIZE)
+        if podaci:
+            strujanje.write(podaci)
+    
+    veza.close()
+    strujanje.stop_stream()
+    strujanje.close()
+    p.terminate()
