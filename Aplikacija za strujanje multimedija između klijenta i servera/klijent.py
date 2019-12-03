98e7724324f68f4f7cf3d26e19f0e7e568552bcb
Jurica Migač <jurica1.migac@gmail.com>
2019-12-03 18:30:10 +0100

    05 Implementiran klijent za slanje multimedije. #10
    Implementirano rješenje za slanje multimedijskog sadržaja od klijenta ka serveru za slušanje pjesme.

--- Aplikacija za strujanje multimedija između klijenta i servera/klijent.py
+++ Aplikacija za strujanje multimedija između klijenta i servera/klijent.py
@@ -4,3 +4,15 @@ import wave
 #Inicijalizacija Klijenta
 HOST = "localhost"
 PORT = 13132
+
+veza = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+veza.connect((HOST,PORT))
+
+wf = wave.open("pjesma.wav", "rb")
+rate = wf.getframerate()
+
+while 1:
+    podaci = wf.readframes(rate*2)
+    veza.sendall(podaci)
+
+veza.close()
