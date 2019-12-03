99147f0098a5b6208819f8fba4c3e4c73972d8bd
Jurica Migač <jurica1.migac@gmail.com>
2019-12-03 18:38:41 +0100

    06 Implementiran server za primanje i reproduciranje pjesme. #9
    Implementiran je server kojim korisnik može prihvatiti jednog korisnika koji se povezao na njega te slušati multimedijski sadržaj koji klijent šalje serveru.

--- Aplikacija za strujanje multimedija između klijenta i servera/server.py
+++ Aplikacija za strujanje multimedija između klijenta i servera/server.py
@@ -13,3 +13,28 @@ p = pyaudio.PyAudio()
 #Konfiguracija socketa
 HOST = "localhost"
 PORT = 13132
+
+strujanje = p.open(
+    format = p.get_format_from_width(WIDTH),
+    channels = KANALI,
+    rate = RATE,
+    output = TRUE,
+    frame_per_buffer = CHUNK
+)
+
+veza = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+veza.bind((HOST,PORT))
+veza.listen(1)
+
+while 1:
+    vezaSocket, adresaKlijenta = veza.accept()
+    data = veza.recv(BUFFER*2)
+    while data != '':
+        strujanje.write(data)
+        data = vezaSocket.recv(BUFFER*2)
+
+vezaSocket.close()
+strujanje.stop_stream()
+strujanje.close()
+p.terminate()
+veza.close()
